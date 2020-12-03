2f8ba22e1b8051c303d9b9f5242fba722b486a04
yenlee38 <ngocyen174308@gmail.com>
2020-12-04 00:19:59 +0700

    them file loginNew.html, mn nho sua lai pass ben trang __init__ nhe

--- BookStore/__init__.py
+++ BookStore/__init__.py
@@ -4,8 +4,8 @@ from flask_admin import Admin
 from flask_login import LoginManager
 app = Flask(__name__)
 
-app.secret_key='ansfhkashfkshfnmsagashfska'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456aA@localhost/BookStore?charset=utf8mb4'
+app.secret_key='ansfhkashfkshfnmsagashfska1111'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:18110402@localhost/BookStore?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app=app)
--- BookStore/main.py
+++ BookStore/main.py
@@ -1,8 +1,9 @@
 from __init__ import app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from admin_models import *
 from __init__ import login
 from models import User
+from flask_login import login_user
 
 @app.route("/")
 def index():
@@ -12,6 +13,21 @@ def index():
 def get_user(user_id):
     return User.query.get(user_id)
 
+
+@app.route('/login', methods = ['get', 'post'])
+def login_admin():
+    err_msg = ""
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter(username == username, password=password).first()
+
+        if user:
+            login_user(user=user)
+        else:
+            err_msg = "Login failed !"
+
+    return redirect('/admin')
 '''hihi thay doi ne'''
 if __name__ == "__main__":
     app.run(port=8999, debug=True) 
